fbfaeb92cafd58d35ca6fa1fdfdfa4a7cd1189c1
Fie-Langmann <113172774+Fie-Langmann@users.noreply.github.com>
2024-04-22 12:20:41 +0200

    Update 5_descriptives.R

--- R/5_descriptives.R
+++ R/5_descriptives.R
@@ -14,9 +14,12 @@ targets::tar_make()
 # Restart session
 source(here::here("R/1_data_start.R"))
 
+# for flowchart
+summary(data$sex)
+
 # Table 1 -----------------------------------------------------------------
 table1 <- data %>%
-  select(gbd, age, sex, yearly_income, education, deprivation, cohabitation, ethnicity, physical_activity, smoking, alcohol_daily, region, bmi30, diabetes, non_cancer_illness, cancer, family_illness) %>%
+  select(gbd, age, sex, yearly_income, education, deprivation, cohabitation, ethnicity, physical_activity, smoking, alcohol_daily, region, bmi30, diabetes, related_disease, cancer, disease_family) %>%
   tbl_summary(by = gbd,
               statistic = list(all_continuous() ~  "{median} ({p10}, {p90})",
                                all_categorical() ~ "{n} ({p}%)"),
