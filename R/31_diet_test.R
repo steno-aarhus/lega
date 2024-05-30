21e0ea8ceb4e36579e0534dbf6194f4ee584e268
Fie Langmann <fielangmann@gmail.com>
2024-05-30 13:25:24 +0000

    Rewrote diet data test script to eliminate the use of dplyr::rowwise(), which worked very slowly

--- R/31_diet_test.R
+++ R/31_diet_test.R
@@ -7,61 +7,63 @@ library(dplyr)
 targets::tar_make()
 source(here::here("R/1_data_start.R"))
 
+# Average weekly dietary intake of food groups -----------------------------------
+# Food groups intakes are summed across rows and divided by the number of 24h recalls
+# completed (p20077) and multiplied with 7 to estimate the weekly intake of each food.
 
-# Average dietary intake of food groups -----------------------------------
-calculate_total <- function(columns) {
-    sum(dplyr::c_across(columns), na.rm = TRUE)
-}
+data <- data %>%
+  # creating food groups from UKB Aurora Perez
+  mutate(
+    legumes_weekly = (rowSums(pick(matches("p26086|p26101|p26136|p26137")), na.rm = TRUE) / p20077) * 7,
+    meats_weekly = (rowSums(pick(matches("p26066|p26100|p26104|p26117|p26122")), na.rm = TRUE) / p20077) * 7,
+    poultry_weekly = (rowSums(pick(matches("p26121|p26069")), na.rm = TRUE) / p20077) * 7,
+    fish_weekly = (rowSums(pick(matches("p26070|p26109|p26132|p26149")), na.rm = TRUE) / p20077) * 7,
+    cereal_refined_weekly = (rowSums(pick(matches("p26113|p26079|p26071|p26072|p26073|p26075|p26068|p26083")), na.rm = TRUE) / p20077) * 7,
+    whole_grain_weekly = (rowSums(pick(matches("p26074|p26076|p26077|p26078|p26105|p26114")), na.rm = TRUE) / p20077) * 7,
+    mixed_dish_weekly = (rowSums(pick(matches("p26128|p26097|p26116|p26135|p26139|p26145")), na.rm = TRUE) / p20077) * 7,
+    dairy_weekly = (rowSums(pick(matches("p26154|p26087|p26096|p26102|p26103|p26099|p26131|p26133|p26150")), na.rm = TRUE) / p20077) * 7,
+    fats_weekly = (rowSums(pick(matches("p26112|p26062|p26063|p26155|p26110|p26111")), na.rm = TRUE) / p20077) * 7,
+    fruit_weekly= (rowSums(pick(matches("p26089|p26090|p26091|p26092|p26093|p26094")), na.rm = TRUE) / p20077) * 7,
+    nut_weekly = (rowSums(pick(matches("p26107|p26108")), na.rm = TRUE) / p20077) * 7,
+    veggie_weekly = (rowSums(pick(matches("p26065|p26098|p26115|p26123|p26125|p26143|p26146|p26147|p26144")), na.rm = TRUE) / p20077) * 7,
+    potato_weekly = (rowSums(pick(matches("p26118|p26119|p26120")), na.rm = TRUE) / p20077) * 7,
+    egg_weekly = (rowSums(pick(matches("p26088")), na.rm = TRUE) / p20077) * 7,
+    non_alc_beverage_weekly = (rowSums(pick(matches("p26124|p26141|p26142|p26148|p26081|p26082|p26095|p26126|p26127")), na.rm = TRUE) / p20077) * 7,
+    alc_beverage_weekly = (rowSums(pick(matches("p26151|p26152|p26153|p26067|p26138")), na.rm = TRUE) / p20077) * 7,
+    snack_weekly = (rowSums(pick(matches("p26106|p26140|p26134|p26084|p26085|p26064|p26080")), na.rm = TRUE) / p20077) * 7,
+    sauce_weekly = (rowSums(pick(matches("p26129|p26130")), na.rm = TRUE) / p20077) * 7,
+    food_weight_weekly = legumes_weekly + meats_weekly + poultry_weekly + fish_weekly + cereal_refined_weekly + whole_grain_weekly +
+      mixed_dish_weekly + dairy_weekly + fats_weekly + fruit_weekly + nut_weekly + veggie_weekly + potato_weekly + egg_weekly +
+      non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly + sauce_weekly,
+    legume_pea_weekly = (rowSums(pick(matches("p26086|p26101|p26136|p26137|peas")), na.rm = TRUE) / p20077) * 7,
+    veggie_pea = ((rowSums(pick(matches("p26065|p26098|p26147|p26123|p26125|p26143|p26146")), na.rm = TRUE) - peas) / p20077) * 7
+   )
 
-calculate_daily <- function(data) {
-    data |>
-        dplyr::mutate(dplyr::across(dplyr::ends_with("_total"), list(daily = ~ .x / p20077))) |>
-        dplyr::rename_with(~ stringr::str_replace("_total_daily", "_daily"))
-}
+# # Drop p-variables for diet ------------------------------------------------------
+# remove_diet <- c(
+#   "p26113", "p26079", "p26071", "p26072", "p26073", "p26075",
+#   "p26068", "p26083", "p26074", "p26076", "p26077", "p26078",
+#   "p26105", "p26114", "p26128", "p26097", "p26116", "p26135",
+#   "p26139", "p26154", "p26087", "p26096", "p26102", "p26103",
+#   "p26099", "p26131", "p26133", "p26150", "p26112", "p26062",
+#   "p26063", "p26155", "p26110", "p26111", "p26089", "p26090",
+#   "p26091", "p26092", "p26093", "p26094", "p26107", "p26108",
+#   "p26065", "p26098", "p26115", "p26123", "p26125", "p26143",
+#   "p26146", "p26147", "p26144", "p26118", "p26119", "p26120",
+#   "p26088", "p26145", "p26124", "p26141", "p26142", "p26148",
+#   "p26081", "p26082", "p26095", "p26126", "p26127", "p26151",
+#   "p26152", "p26153", "p26067", "p26138", "p26106", "p26140",
+#   "p26134", "p26084", "p26085", "p26064", "p26080", "p26129",
+#   "p26130", "p26086", "p26101", "p26136", "p26137", "p26066",
+#   "p26100", "p26104", "p26117", "p26122", "p26121", "p26069",
+#   "p26070", "p26109", "p26132", "p26149", "p26000"
+# )
+#
+# data <- data %>%
+#   calculate_food_intake() %>%
+#   select(-matches(remove_diet))
 
-calculate_weekly <- function(data) {
-    data |>
-        dplyr::mutate(dplyr::across(dplyr::ends_with("_total"), list(weekly = ~ (.x / p20077) * 7))) |>
-        dplyr::rename_with(~ stringr::str_replace("_total_weekly", "_weekly"))
-}
+# # # Save data ---------------------------------------------------------------
+# arrow::write_parquet(data, here("data/data.parquet"))
+# ukbAid::upload_data(here("data/data.parquet"), username = "FieLangmann")
 
-calculate_food_intake <- function(data) {
-    # estimating average daily and weekly intakes of food groups in g
-    data <- data %>%
-        # creating food groups from UKB Aurora Perez
-        rowwise() |>
-        mutate(
-            cereal_refined_total = calculate_total(matches("p26113|p26079|p26071|p26072|p26073|p26075|p26068|p26083")),
-            whole_grain_total = calculate_total(matches("p26074|p26076|p26077|p26078|p26105|p26114"))
-        ) |>
-        calculate_daily() |>
-        calculate_weekly() |>
-        ungroup()
-    return(diet_data)
-}
-
-
-
-diet <- calculate_food_intake(data)
-
-calculate_food_intake <- function(data) {
-    # estimating average daily and weekly intakes of food groups in g
-  data <- data |>
-    mutate(
-      cereal_refined_total = rowSums(pick(matches("p26113|p26079|p26071|p26072|p26073|p26075|p26068|p26083")")), na.rm = TRUE),
-      whole_grain_total = rowSums(pick(matches("p26074|p26076|p26077|p26078|p26105|p26114")), na.rm = TRUE))
-            ) |>
-        calculate_daily() |>
-        calculate_weekly() |>
-        ungroup()
-    return(diet_data)
-}
-
-
-
-# Fra legliv
-data <- data |>
-    mutate(
-      hummus_guac = rowSums(pick(matches("p26144")), na.rm = TRUE) / 2,
-      peas_corn = rowSums(pick(matches("p26115")), na.rm = TRUE) / 2,
-      legume_daily = rowSums(pick(matches("p26086|p26101|p26136|p26137|hummus_guac|peas_corn")), na.rm = TRUE) / p20077
