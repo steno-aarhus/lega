979939c3ac076f11414ffaf14760d568df0861a4
Fie Langmann <fielangmann@gmail.com>
2024-06-17 08:17:07 +0000

    updated to save and load data from RAP after data wrangling of raw data

--- data-raw/processing.R
+++ data-raw/processing.R
@@ -2,23 +2,17 @@
 # Load packages -----------------------------------------------------------
 library(tidyverse)
 
-# Load saved data:
-# source(here::here("R/1_data_start.R")) #loads data
-# source(here::here("R/2_data_management.R")) #runs data management
-# source(here::here("R/3_diet_data.R")) #runs diet code
-# source(here::here("R/4_icd10_outcomes_tte.R")) #runs outcome code
+# Save data to RAP --------------------------------------------------------
 
-# Save the changes as parquet and upload to the RAP folder for easy download
-# next time you sign in
-# arrow::write_parquet(data, here("data/data.parquet"))
+# save data locally to be able to upload to RAP
+readr::write_csv(data, here::here("data/data_lega.csv"))
 
-# Upload to the project RAP folder.
-# ukbAid::upload_data(here("data/data.parquet"), username = "FieLangmann")
-
-# Now you can run your analyses with data as data frame
+# save to RAP
+rap_copy_to("data/data_lega.csv", "/users/FieLangmann/data_lega.csv")
 
 
-# Save data to RAP after data management
-# Find RAP path
-rap_get_path_dirs("/users") # add the username for the full path into rap_copy_to()
-rap_copy_to("data/data.csv", "/users/FieLangmann/data_lega.csv")
+# Load data from RAP ------------------------------------------------------
+
+# load data from RAP
+data <- readr::read_csv("/users/FieLangmann/data_lega.csv")
+
