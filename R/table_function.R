e3a5f789784204e10ff1d40e8a685d8f270c3b31
Fie Langmann <fielangmann@gmail.com>
2024-09-12 11:05:39 +0000

    minor edits

--- R/table_function.R
+++ R/table_function.R
@@ -70,16 +70,4 @@ table_df %>% gt() %>%
     gtsave("main_table.html")
 
 flextable::save_as_html(table1, path = here("doc", "table1.html"))
-# From Luke:
-# Instead of working with three (or more) dataframes, combine them together into
-# one dataframe via bind_rows() or similar and then use filter() to take out what
-# you want.
 
-#
-# table_df <- data.frame(
-#   model = c("Model 1", "Model 2", "Model 3"),
-#   meat = c(data$meat1),
-#   poultry = c(meat1, meat2, meat3),
-#   fish = c(meat1, meat2, meat3)
-# )
-# knitr::kable(table_df, col.names = c("Statistical model", "Red and processed meat"), row.names = TRUE)
