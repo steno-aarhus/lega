268280e28487f7ce0ffbf60867cd3b92c675a780
Fie Langmann <fielangmann@gmail.com>
2024-11-28 08:41:03 +0000

    minor edits

--- R/extra_analyses.R
+++ R/extra_analyses.R
@@ -67,51 +67,14 @@ cholecystit_model2<- function(data) {
 }
 
 ## Sex strata any GBD ---------------------------------------------------------
-sex_strata <- function(data) {
-    # Define covariates and models
-    covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
-                 "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
-                 "veggie_weekly", "potato_weekly", "egg_weekly",
-                 "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
-                 "sauce_weekly", "food_weight_weekly", "ethnicity",
-                 "deprivation", "education", "cohabitation", "physical_activity",
-                 "smoking", "estrogen_treatment", "bilirubin", "weight_loss",
-                 "pregnancies", "yearly_income",
-                 "related_conditions", "family_diabetes",
-                 "strata(region, age_strata)")
-
-    model2_formulas <- list(
-        meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covars2),
-        poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covars2),
-        fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
-    )
-
-    # Function to run analysis for a given sex
-    run_analysis <- function(sex_group) {
-        subset_data <- data %>%
-            subset(sex == sex_group)
-
-        results <- model2_formulas |>
-            map(~ coxph(.x, data = subset_data, ties = "breslow")) |>
-            map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
-                     mutate(across(where(is.numeric), ~ round(.x, 2))) |>
-                     mutate(model = .y, sex = ifelse(sex_group == 1, "men", "women")))
-
-        return(results)
-    }
-
-    # Apply the analysis for both sexes
-    all_results <- map(0:1, run_analysis) |>
-        bind_rows()
-
-    return(all_results)
+create_formula <- function(xvars, covars) {
+    outcome <- "Surv(survival_gbd, gbd == 1)"
+    reformulate(c(xvars, covars), response = outcome)
 }
-
-
 
 men_strata <- function(data) {
     men <- data %>%
-        subset(sex == 1)
+        filter(sex == 0)
     covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
                  "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
                  "veggie_weekly", "potato_weekly", "egg_weekly",
@@ -129,13 +92,13 @@ men_strata <- function(data) {
         fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
     )
 
-    model2_results <- model2_formulas |>
+    model2_men <- model2_formulas |>
         map(~ coxph(.x, data = men, ties = "breslow")) |>
         map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
                  mutate(across(where(is.numeric), ~ round(.x, 2))) |>
                  mutate(model = .y))
 
-    return(model2_results)
+    return(model2_men)
 }
 
 women_strata <- function(data) {
@@ -215,7 +178,7 @@ bmi_strata<- function(data) {
     data <- data %>%
         mutate(bmi_strata =
                    case_when(bmi < 25 ~ 0,
-                             bmi >= 25 % bmi < 30 ~ 1,
+                             bmi >= 25 & bmi < 30 ~ 1,
                              bmi > 30 ~ 2))
 
     covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
