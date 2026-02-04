f4a3cb1d97b660d6473fa786780fe952600aec65
Fie Langmann <fielangmann@gmail.com>
2026-02-04 12:15:09 +0000

    estimating associations when removing unknown family history

--- R/additional_calculations.R
+++ R/additional_calculations.R
@@ -156,3 +156,57 @@ fish_splines <- coxph(Surv(survival_gbd, event = gbd) ~ fish_spline + ethnicity
 
 # Perform a likelihood ratio test
 lrt_result <- anova(fish, fish_splines, test = "LRT") %>% print() # p = 0.59
+
+
+
+
+# Unknown family disease history ------------------------------------------
+
+# Confounder issues -------------------------------------------------------
+library(magrittr)
+library(dplyr)
+library(survival)
+library(broom)
+library(purrr)
+
+# How many unknown family disease history?
+data <- targets::tar_read(sorted_data)
+table(data$disease_family)
+
+# Excluding unknown family history
+known_history <- data %>%
+    filter(disease_family != "unknown")
+
+create_formula <- function(xvars, covars) {
+    outcome <- "Surv(survival_gbd, gbd == 1)"
+    reformulate(c(xvars, covars), response = outcome)
+}
+
+main_model2<- function(data) {
+    covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+                 "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+                 "veggie_weekly", "potato_weekly", "egg_weekly",
+                 "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+                 "sauce_weekly", "food_weight_weekly", "ethnicity",
+                 "deprivation", "education", "cohabitation", "physical_activity",
+                 "smoking", "estrogen_treatment", "bilirubin", "weight_loss",
+                 "pregnancies", "yearly_income",
+                 "related_conditions", "family_diabetes",
+                 "strata(region, age_strata, sex)")
+
+    model2_formulas <- list(
+        meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covars2),
+        poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covars2),
+        fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
+    )
+
+    model2_results <- model2_formulas |>
+        map(~ survival::coxph(.x, data = data, ties = "breslow")) |>
+        map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
+                 mutate(across(where(is.numeric), ~ round(.x, 2))) |>
+                 mutate(model = .y))
+
+    return(model2_results)
+}
+
+known <- main_model2(known_history)
