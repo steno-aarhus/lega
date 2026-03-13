384c083e85c25ba93ea8569fa9c97d8f1ecac71c
Fie Langmann <fielangmann@gmail.com>
2026-03-13 11:58:58 +0100

    Cleaning up script

--- R/analyses.R
+++ R/analyses.R
@@ -458,82 +458,83 @@ women_strata <- function(data) {
 
 # Main analyses stratifying  by age
 
-# ## Age strata any GBD ---------------------------------------------------------
-# age_strata <- function(data) {
-#     data <- data %>%
-#         mutate(age_tertiles = ntile(age, 3))
-#     tertile_age <- quantile(data$age, probs = c(0, 1/3, 2/3, 1))
-#
-#     tertile1 = data %>%
-#         subset(age_tertiles == 1)
-#     tertile2 = data %>%
-#         subset(age_tertiles == 2)
-#     tertile3 = data %>%
-#         subset(age_tertiles == 3)
-#
-#     above57 <- data %>%
-#         subset(age >= 57)
-#     below57 <- data %>%
-#         subset(age < 57)
-#     covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
-#                  "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
-#                  "veggie_weekly", "potato_weekly", "egg_weekly",
-#                  "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
-#                  "sauce_weekly", "food_weight_weekly", "ethnicity",
-#                  "deprivation", "education", "cohabitation", "physical_activity",
-#                  "smoking", "estrogen_treatment", "bilirubin", "weight_loss",
-#                  "pregnancies", "yearly_income",
-#                  "related_conditions", "family_diabetes",
-#                  "strata(region, sex)")
-#
-#     model2_formulas <- list(
-#         meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covars2),
-#         poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covars2),
-#         fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
-#     )
-#
-#     model2_tertile1 <- model2_formulas |>
-#         map(~ coxph(.x, data = tertile1, ties = "breslow")) |>
-#         map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
-#                  mutate(across(where(is.numeric), ~ round(.x, 2))) |>
-#                  mutate(model = .y))
-#
-#     model2_tertile2 <- model2_formulas |>
-#         map(~ coxph(.x, data = tertile2, ties = "breslow")) |>
-#         map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
-#                  mutate(across(where(is.numeric), ~ round(.x, 2))) |>
-#                  mutate(model = .y))
-#
-#     model2_tertile3 <- model2_formulas |>
-#         map(~ coxph(.x, data = tertile3, ties = "breslow")) |>
-#         map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
-#                  mutate(across(where(is.numeric), ~ round(.x, 2))) |>
-#                  mutate(model = .y))
-#
-#     model2_below57 <- model2_formulas |>
-#         map(~ coxph(.x, data = below57, ties = "breslow")) |>
-#         map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
-#                  mutate(across(where(is.numeric), ~ round(.x, 2))) |>
-#                  mutate(model = .y))
-#
-#
-#     model2_above57 <- model2_formulas |>
-#         map(~ coxph(.x, data = above57, ties = "breslow")) |>
-#         map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
-#                  mutate(across(where(is.numeric), ~ round(.x, 2))) |>
-#                  mutate(model = .y))
-#
-#
-#     # Combine all results into a list and return it
-#     return(list(
-#         tertile1 = model2_tertile1,
-#         tertile2 = model2_tertile2,
-#         tertile3 = model2_tertile3,
-#         below57 = model2_below57,
-#         above57 = model2_above57
-#     ))
-# }
-#
+age_strata <- function(data) {
+    data <- data %>%
+        mutate(age_tertiles = ntile(age, 3))
+    tertile_age <- quantile(data$age, probs = c(0, 1/3, 2/3, 1))
+
+    tertile1 = data %>%
+        subset(age_tertiles == 1)
+    tertile2 = data %>%
+        subset(age_tertiles == 2)
+    tertile3 = data %>%
+        subset(age_tertiles == 3)
+
+    above57 <- data %>%
+        subset(age >= 57)
+    below57 <- data %>%
+        subset(age < 57)
+    covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+                 "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+                 "veggie_weekly", "potato_weekly", "egg_weekly",
+                 "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+                 "sauce_weekly", "food_weight_weekly", "ethnicity",
+                 "deprivation", "education", "cohabitation", "physical_activity",
+                 "smoking", "estrogen_treatment", "bilirubin", "weight_loss",
+                 "pregnancies", "yearly_income",
+                 "related_conditions", "family_diabetes",
+                 "strata(region, sex)")
+
+    model2_formulas <- list(
+        meat_model2 = create_formula(c("legumes80", "poultry80", "fish80"), covars2),
+        poultry_model2 = create_formula(c("legumes80", "meats80", "fish80"), covars2),
+        fish_model2 = create_formula(c("legumes80", "meats80", "poultry80"), covars2)
+    )
+
+    model2_tertile1 <- model2_formulas |>
+        map(~ coxph(.x, data = tertile1, ties = "breslow")) |>
+        map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
+                 mutate(across(where(is.numeric), ~ round(.x, 2))) |>
+                 mutate(model = .y))
+
+    model2_tertile2 <- model2_formulas |>
+        map(~ coxph(.x, data = tertile2, ties = "breslow")) |>
+        map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
+                 mutate(across(where(is.numeric), ~ round(.x, 2))) |>
+                 mutate(model = .y))
+
+    model2_tertile3 <- model2_formulas |>
+        map(~ coxph(.x, data = tertile3, ties = "breslow")) |>
+        map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
+                 mutate(across(where(is.numeric), ~ round(.x, 2))) |>
+                 mutate(model = .y))
+
+    model2_below57 <- model2_formulas |>
+        map(~ coxph(.x, data = below57, ties = "breslow")) |>
+        map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
+                 mutate(across(where(is.numeric), ~ round(.x, 2))) |>
+                 mutate(model = .y))
+
+
+    model2_above57 <- model2_formulas |>
+        map(~ coxph(.x, data = above57, ties = "breslow")) |>
+        map2(names(model2_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
+                 mutate(across(where(is.numeric), ~ round(.x, 2))) |>
+                 mutate(model = .y))
+
+
+    # Combine all results into a list and return it
+    return(list(
+        tertile1 = model2_tertile1,
+        tertile2 = model2_tertile2,
+        tertile3 = model2_tertile3,
+        below57 = model2_below57,
+        above57 = model2_above57
+    ))
+}
+
+
+# Main analyses stratifying  by BMI
 # ## BMI strata any GBD ---------------------------------------------------------
 # bmi_strata<- function(data) {
 #     bmi_normal <- data %>%
