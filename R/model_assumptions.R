70056a691c67fa52e9966dfc0512a15bd71f1f00
Fie-Langmann <113172774+Fie-Langmann@users.noreply.github.com>
2026-03-13 10:00:24 +0100

    Minor edits
    Commented out unused code related to models.

--- R/model_assumptions.R
+++ R/model_assumptions.R
@@ -33,7 +33,7 @@ model_assumption <- function(data) {
   # poultry
   poultry_model2 <- coxph(
     Surv(survival_gbd, gbd == 1) ~
-      # removing meat
+      # removing poultry
       legumes80 + meats80 + fish80 +
       # other food components
       cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
@@ -56,7 +56,7 @@ model_assumption <- function(data) {
   # fish
   fish_model2 <- coxph(
     Surv(survival_gbd, gbd == 1) ~
-      # removing meat
+      # removing fish
       legumes80 + meats80 + poultry80 +
       # other food components
       cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
@@ -77,83 +77,83 @@ model_assumption <- function(data) {
   return(data)
 }
 
-model_assumption(data)
+
+# data <- data %>%
+#    mutate(
+#        legumes80 = legume_weekly / 80,
+#        meats80 = meats_weekly / 80,
+#        poultry80 = poultry_weekly / 80,
+#        fish80 = fish_weekly / 80
+#    )
+# meat_model2 <- coxph(
+#    Surv(survival_gbd, gbd == 1) ~
+#        # removing meat
+#        legumes80 + poultry80 + fish80 +
+#       # other food components
+#        cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
+#        dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
+#        veggie_weekly + potato_weekly + egg_weekly +
+#        non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
+#        sauce_weekly + food_weight_weekly +
+#        # other variables
+#        ethnicity + deprivation + education +
+#        physical_activity + smoking +
+#        estrogen_treatment + bilirubin + weight_loss +
+#        yearly_income +
+#       pregnancies + related_conditions + family_diabetes,
+#    data = data, ties = "breslow"
+# )
+
+# ph_test1 <- cox.zph(meat_model2)
+# print(ph_test1)
+# plot(ph_test1)
+
+# # poultry
+# poultry_model2 <- coxph(
+#    Surv(survival_gbd, gbd == 1) ~
+#        # removing meat
+#        legumes80 + meats80 + fish80 +
+#        # other food components
+#        cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
+#        dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
+#        veggie_weekly + potato_weekly + egg_weekly +
+#        non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
+#        sauce_weekly + food_weight_weekly +
+#        # other variables
+#        ethnicity + deprivation + education +
+#        physical_activity + smoking +
+#        estrogen_treatment + bilirubin + weight_loss +
+#        yearly_income +
+#        pregnancies + related_conditions + family_diabetes,
+#    data = data, ties = "breslow"
+# )
+
+# ph_test2 <- cox.zph(poultry_model2)
+# print(ph_test2)
+# plot(ph_test2)
+
+# # fish
+# fish_model2 <- coxph(
+#    Surv(survival_gbd, gbd == 1) ~
+#        # removing meat
+#        legumes80 + meats80 + poultry80 +
+#        # other food components
+#        cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
+#        dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
+#        veggie_weekly + potato_weekly + egg_weekly +
+#        non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
+#        sauce_weekly + food_weight_weekly +
+#        # other variables
+#        ethnicity + deprivation + education +
+#        physical_activity + smoking +
+#        estrogen_treatment + bilirubin + weight_loss +
+#        yearly_income +
+#        pregnancies + related_conditions + family_diabetes,
+#    data = data, ties = "breslow"
+# )
+
+# ph_test3 <- cox.zph(fish_model2)
+# print(ph_test3)
+# plot(ph_test3)
 
 
-data <- data %>%
-    mutate(
-        legumes80 = legume_weekly / 80,
-        meats80 = meats_weekly / 80,
-        poultry80 = poultry_weekly / 80,
-        fish80 = fish_weekly / 80
-    )
-meat_model2 <- coxph(
-    Surv(survival_gbd, gbd == 1) ~
-        # removing meat
-        legumes80 + poultry80 + fish80 +
-        # other food components
-        cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-        dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-        veggie_weekly + potato_weekly + egg_weekly +
-        non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-        sauce_weekly + food_weight_weekly +
-        # other variables
-        ethnicity + deprivation + education +
-        physical_activity + smoking +
-        estrogen_treatment + bilirubin + weight_loss +
-        yearly_income +
-        pregnancies + related_conditions + family_diabetes,
-    data = data, ties = "breslow"
-)
-
-ph_test1 <- cox.zph(meat_model2)
-print(ph_test1)
-plot(ph_test1)
-
-# poultry
-poultry_model2 <- coxph(
-    Surv(survival_gbd, gbd == 1) ~
-        # removing meat
-        legumes80 + meats80 + fish80 +
-        # other food components
-        cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-        dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-        veggie_weekly + potato_weekly + egg_weekly +
-        non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-        sauce_weekly + food_weight_weekly +
-        # other variables
-        ethnicity + deprivation + education +
-        physical_activity + smoking +
-        estrogen_treatment + bilirubin + weight_loss +
-        yearly_income +
-        pregnancies + related_conditions + family_diabetes,
-    data = data, ties = "breslow"
-)
-
-ph_test2 <- cox.zph(poultry_model2)
-print(ph_test2)
-plot(ph_test2)
-
-# fish
-fish_model2 <- coxph(
-    Surv(survival_gbd, gbd == 1) ~
-        # removing meat
-        legumes80 + meats80 + poultry80 +
-        # other food components
-        cereal_refined_weekly + whole_grain_weekly + mixed_dish_weekly +
-        dairy_weekly + fats_weekly + fruit_weekly + nut_weekly +
-        veggie_weekly + potato_weekly + egg_weekly +
-        non_alc_beverage_weekly + alc_beverage_weekly + snack_weekly +
-        sauce_weekly + food_weight_weekly +
-        # other variables
-        ethnicity + deprivation + education +
-        physical_activity + smoking +
-        estrogen_treatment + bilirubin + weight_loss +
-        yearly_income +
-        pregnancies + related_conditions + family_diabetes,
-    data = data, ties = "breslow"
-)
-
-ph_test3 <- cox.zph(fish_model2)
-print(ph_test3)
-plot(ph_test3)
