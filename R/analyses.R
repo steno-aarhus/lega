f1b89673b5ea156b90a7e3dce298b49b6881533e
Fie Langmann <fielangmann@gmail.com>
2026-03-13 11:52:58 +0100

    Cleaning up script

--- R/analyses.R
+++ R/analyses.R
@@ -281,46 +281,51 @@ covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
 
 
 
-# # Extra analyses ---------------------------------------------------------
-# ## Gallstones only ---------------------------------------------------------
-# create_formula_gallstone <- function(xvars, covars) {
-#     outcome <- "Surv(survival_gallstone, gallstone == 1)"
-#     reformulate(c(xvars, covars), response = outcome)
-# }
-#
-# gallstone_model2<- function(data) {
-#     covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
-#                  "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
-#                  "veggie_weekly", "potato_weekly", "egg_weekly",
-#                  "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
-#                  "sauce_weekly", "food_weight_weekly", "ethnicity",
-#                  "deprivation", "education", "cohabitation", "physical_activity",
-#                  "smoking", "estrogen_treatment", "bilirubin", "weight_loss",
-#                  "pregnancies", "yearly_income",
-#                  "related_conditions", "family_diabetes",
-#                  "strata(region, age_strata, sex)")
-#
-#     model2_gallstone_formulas <- list(
-#         meat_model2 = create_formula_gallstone(c("legumes80", "poultry80", "fish80"), covars2),
-#         poultry_model2 = create_formula_gallstone(c("legumes80", "meats80", "fish80"), covars2),
-#         fish_model2 = create_formula_gallstone(c("legumes80", "meats80", "poultry80"), covars2)
-#     )
-#
-#     model2_gallstone <- model2_gallstone_formulas |>
-#         map(~ coxph(.x, data = data, ties = "breslow")) |>
-#         map2(names(model2_gallstone_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
-#                  mutate(across(where(is.numeric), ~ round(.x, 2))) |>
-#                  mutate(model = .y))
-#
-#     return(model2_gallstone)
-# }
-#
-#
-# ## Cholecystectomy only --------------------------------------------------------
-# create_formula_removal <- function(xvars, covars) {
-#     outcome <- "Surv(survival_removal, cholecystectomy == 1)"
-#     reformulate(c(xvars, covars), response = outcome)
-# }
+# Extra analyses ---------------------------------------------------------
+
+## Splitting gallbladder diseases into distinct outcomes: gallstone, cholecystectomy,
+# and cholecystitis
+
+
+# Gallstones only
+create_formula_gallstone <- function(xvars, covars) {
+    outcome <- "Surv(survival_gallstone, gallstone == 1)"
+    reformulate(c(xvars, covars), response = outcome)
+}
+
+gallstone_model2<- function(data) {
+    covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
+                 "dairy_weekly", "fats_weekly", "fruit_weekly", "nut_weekly",
+                 "veggie_weekly", "potato_weekly", "egg_weekly",
+                 "non_alc_beverage_weekly", "alc_beverage_weekly", "snack_weekly",
+                 "sauce_weekly", "food_weight_weekly", "ethnicity",
+                 "deprivation", "education", "cohabitation", "physical_activity",
+                 "smoking", "estrogen_treatment", "bilirubin", "weight_loss",
+                 "pregnancies", "yearly_income",
+                 "related_conditions", "family_diabetes",
+                 "strata(region, age_strata, sex)")
+
+    model2_gallstone_formulas <- list(
+        meat_model2 = create_formula_gallstone(c("legumes80", "poultry80", "fish80"), covars2),
+        poultry_model2 = create_formula_gallstone(c("legumes80", "meats80", "fish80"), covars2),
+        fish_model2 = create_formula_gallstone(c("legumes80", "meats80", "poultry80"), covars2)
+    )
+
+    model2_gallstone <- model2_gallstone_formulas |>
+        map(~ coxph(.x, data = data, ties = "breslow")) |>
+        map2(names(model2_gallstone_formulas), ~ tidy(.x, exponentiate = TRUE, conf.int = TRUE) |>
+                 mutate(across(where(is.numeric), ~ round(.x, 2))) |>
+                 mutate(model = .y))
+
+    return(model2_gallstone)
+}
+
+# Cholecystectomy only
+
+create_formula_removal <- function(xvars, covars) {
+    outcome <- "Surv(survival_removal, cholecystectomy == 1)"
+    reformulate(c(xvars, covars), response = outcome)
+}
 #
 # removal_model2<- function(data) {
 #     covars2 <- c("cereal_refined_weekly", "whole_grain_weekly", "mixed_dish_weekly",
